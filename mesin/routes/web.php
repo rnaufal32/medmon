8aa3dcedfadad843cd89be95830ebfbc3491c3d5
naufal <rnaufal32@muslim.com>
2025-02-01 07:17:12 +0700

    fix remove unused

--- mesin/routes/web.php
+++ mesin/routes/web.php
@@ -12,7 +12,7 @@ Route::post('signin', [\App\Http\Controllers\Auth\SignInController::class, 'stor
 Route::group([
     'middleware' => 'auth'
 ], function () {
-    Route::get('dashboard', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('dashboard.index');
     Route::get('/mentions', [\App\Http\Controllers\MentionController::class, 'index'])->name('mentions.index');
     Route::get('/summary', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('summary.index');
     Route::get('/sentiment', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('sentiment.index');
@@ -21,9 +21,18 @@ Route::group([
     // UTILITY
     Route::post('/select-type-media', [\App\Http\Controllers\SelectTypeMediaController::class, 'store'])->name('select-type-media.store');
 
-    Route::get('signout', function () {
+    Route::get('/signout', function () {
         \Illuminate\Support\Facades\Auth::logout();
 
         return redirect('/signin');
     })->name('signout');
+
+    Route::get('/roles', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('roles.index');
+    Route::get('/permissions', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('permissions.index');
+    Route::get('/users', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('users.index');
+    Route::get('/menus', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('menus.index');
+    Route::get('/scrape', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('scrape.index');
+    Route::get('/keywords', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('keywords.index');
+    Route::get('/news', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('news.index');
+    Route::get('/crawling', [\App\Http\Controllers\Client\DashboardController::class, 'index'])->name('crawling.index');
 });
